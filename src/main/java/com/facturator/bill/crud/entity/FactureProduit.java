b07aa993dfd6d7d1d07316df65c1a9a2bdf63023
Francois-Developpeur <101627023+Francois-Developpeur@users.noreply.github.com>
2022-10-04 12:04:09 +0200

    Add files via upload

--- src/main/java/com/facturator/bill/crud/entity/FactureProduit.java
+++ src/main/java/com/facturator/bill/crud/entity/FactureProduit.java
@@ -1,7 +1,11 @@
-/*package com.facturator.bill.crud.entity;
+package com.facturator.bill.crud.entity;
+
+import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -11,15 +15,17 @@ import javax.persistence.Table;
 @Table(name="facture_produit")	
 public class FactureProduit {
 	
-	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id	
+	private int fcId;
+	
 	@ManyToOne
 	@JoinColumn(name="fac_numero")
-	private int fcNumero;
-	
-	@Id
+	private Facture fcNumero;
+
 	@ManyToOne
 	@JoinColumn(name="pro_reference")
-	private int fcReference;
+	private Produit fcReference;
 	
 	@Column(name="fc_quantite")
 	private Double fcQuantite;
@@ -28,25 +34,34 @@ public class FactureProduit {
 		
 	}
 
-	public FactureProduit(int fcNumero, int fcReference, Double fcQuantite) {
+	public FactureProduit(int fcId, Facture fcNumero, Produit fcReference, Double fcQuantite) {
+		this.fcId = fcId;
 		this.fcNumero = fcNumero;
 		this.fcReference = fcReference;
 		this.fcQuantite = fcQuantite;
 	}
 
-	public int getFcNumero() {
+	public int getFcId() {
+		return fcId;
+	}
+
+	public void setFcId(int fcId) {
+		this.fcId = fcId;
+	}
+
+	public Facture getFcNumero() {
 		return fcNumero;
 	}
 
-	public void setFcNumero(int fcNumero) {
+	public void setFcNumero(Facture fcNumero) {
 		this.fcNumero = fcNumero;
 	}
 
-	public int getFcReference() {
+	public Produit getFcReference() {
 		return fcReference;
 	}
 
-	public void setFcReference(int fcReference) {
+	public void setFcReference(Produit fcReference) {
 		this.fcReference = fcReference;
 	}
 
@@ -60,9 +75,8 @@ public class FactureProduit {
 
 	@Override
 	public String toString() {
-		return "FactureProduit [fcNumero=" + fcNumero + ", fcReference=" + fcReference + ", fcQuantite=" + fcQuantite
-				+ "]";
+		return "FactureProduit [fcId=" + fcId + ", fcNumero=" + fcNumero + ", fcReference=" + fcReference
+				+ ", fcQuantite=" + fcQuantite + "]";
 	}
 
 }
-*/
